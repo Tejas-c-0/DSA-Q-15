07b11fcb4159df4f8d3789ae2a7263f8ba2e7f79
Tejas C <shobha2ganesh@gmail.com>
2026-04-25 19:34:14 +0530

    Best time to buy and sell stock

---
+++ Best time to buy and sell stock.py
@@ -0,0 +1,12 @@
+prices = [7,1,5,3,6,4]
+def maxProfit(prices):
+    min_price = float('inf')
+    max_profit = 0
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        profit = price - min_price
+        if profit > max_profit:
+            max_profit = profit
+    return max_profit
+print(maxProfit(prices))
